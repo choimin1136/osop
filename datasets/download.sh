cf5ca3238c15b9eeab2e2fb7d813e12e9d645e33
choimin1136 <choimin1136@gmail.com>
2023-11-25 18:14:58 +0900

    Update download.sh
    window cmd prompt is working tar
    unzip -> tar

--- datasets/download.sh
+++ datasets/download.sh
@@ -4,9 +4,9 @@ wget http://images.cocodataset.org/zips/train2017.zip -O coco_train2017.zip
 wget http://images.cocodataset.org/zips/val2017.zip -O coco_val2017.zip
 wget http://images.cocodataset.org/zips/test2017.zip -O coco_test2017.zip
 
-unzip coco_train2017.zip
-unzip coco_val2017.zip
-unzip coco_test2017.zip
+tar -xf coco_train2017.zip
+tar -xf coco_val2017.zip
+tar -xf coco_test2017.zip
 
 rm coco_train2017.zip
 rm coco_val2017.zip
@@ -17,8 +17,8 @@ cd ..
 wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip -O coco_ann2017.zip
 wget http://images.cocodataset.org/annotations/image_info_test2017.zip -O coco_t_ann2017.zip
 
-unzip coco_ann2017.zip
-unzip coco_t_ann2017.zip
+tar -xf coco_ann2017.zip
+tar -xf coco_t_ann2017.zip
 
 rm coco_ann2017.zip
 rm coco_t_ann2017.zip
